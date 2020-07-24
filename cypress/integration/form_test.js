bc4e5476bd0d32a9919ffa243eb654f38f2ecb7a
Dee <47228065+DeeDowns@users.noreply.github.com>
2020-07-24 14:15:39 -0400

    created and passed second test

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -11,3 +11,26 @@ describe('can add text to the name input', () => {
         cy.get('input[name="name"]').type('Dee')
     })
 })
+
+
+describe('can select multiple toppings', () => {
+    it('can navigate to the site', () => {
+        cy.visit('http://localhost:3000/')
+        cy.url().should('include', 'localhost')
+    })
+    it('can navigate to the form page through the to form button', () => {
+        cy.get('button').click()
+    })
+    it('can get pepperoni input and select it', () => {
+        cy.get('input[name="pepperoni"]').click()
+    })
+    it('can get sausage input and select it', () => {
+        cy.get('input[name="sausage"]').click()
+    })
+    it('can get anchovies input and select it', () => {
+        cy.get('input[name="anchovies"]').click()
+    })
+    it('can get extraCheese input and select it', () => {
+        cy.get('input[name="extraCheese"]').click()
+    })
+})
